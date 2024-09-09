fc01eab892dcf38250f3fa22268c353984ed6de3
Nicolas Cianni <nicocianni21@gmail.com>
2024-09-09 13:28:51 -0300

    src-route-img

--- src/GlobalHeroSection.jsx
+++ src/GlobalHeroSection.jsx
@@ -8,7 +8,7 @@ export function GlobalHero () {
         <section className="hero-section">
             <motion.img 
             className="hero-img" 
-            src="./assets/newphoto.jpg"
+            src="../src/assets/newphoto.jpg"
             initial = {{x : -200, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{duration : 1.5}}
